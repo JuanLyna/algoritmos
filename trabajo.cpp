c485fab58c7261e3070ee7216ac39ccb13b430c6
Jean Luna <104095517+JuanLyna@users.noreply.github.com>
2023-02-24 02:36:00 -0300

    resubo el trabajo

--- trabajo.cpp
+++ trabajo.cpp
@@ -1,104 +1,114 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-/*función que examina los datos */
+
 const int  clients {8} ;
 const int products {5} ; 
-//falta hacer el punto 2 y 3  , tienes que hayar una forma de ordernar los km y buscar la inforamción del producto al mismo tiempo 
-void order_double(float m[clients] [products] , int c , string n[clients] , string type_products[clients] , int entregas[clients] ,int e[clients]) //está función solo ordena los km y que se vaya ordenando con los productos y entregas 
+
+void order_double(double m[clients] [products] , double distanceOrd [products] , string productsOrd[products] , string type_products[products] , int clientUp ) //está función solo ordena los km y que se vaya ordenando con los productos y entregas 
 {   
-   int temp  ;
-    
-        for (int  i = 0; i < products ;i++)
-        {
-            for (int j = 0; j < products ; j++)
-            {
-                if(m[c][j]>m[c][j+1])
-                {
-                    temp= m[c][j] ;
-                    m[c][j] = m[c][j+1] ;
-                    m[c][j+1] =temp ; 
+   
+   for (int i = 0; i < products-1; i++)//hacemos esto para no dañar el array original
+   {
+        productsOrd[i] = type_products[i] ; 
+        distanceOrd[i] = m[clientUp][i] ; 
 
-                    n[j]=type_products[j+1] ;
-                    n[j+i] = type_products [j] ;
+   };
+   
+   
+   
+   for(int i=0; i<products;i++){
+		for(int j = 0; j < products-1; j++){
+				if(distanceOrd[j]> distanceOrd[j+1]){
+					double temp=distanceOrd[j];
+					distanceOrd[j] = distanceOrd[i+1];
+					distanceOrd[j+1] = temp;
+					string tempS = productsOrd[j];
+					productsOrd[i] = productsOrd[i+1];
+					productsOrd[i+1] = tempS;
 
-                    e[j] = entregas [j+1] ; 
-                    e[j+1] = entregas[j];
-                }
-            }
-            
-        }
+				}
+		}
+	}
+   
+   
+   
         
 }
 
-void getTrans (float pesoTrans[clients][products]  , string name_clients[clients] ,string type_products[products] , float distanceRecorry[clients][products] , int entregas[products]) 
 
-{   int count , mayor=0 , clientPosition ; //el clientPosition sirve para el punto 2 , el count y mayor es para saber cuantas entregas de ese producto hizo ese cliente y si fue el que mayor hizo entregas superando los 1300kg 
-    int e[clients] ; //punto 2 para evitar desordenar el array de entregas
-    string n[products] ; //punto 2  para evitar desordenar el array de producos
+void getTransBetter (double pesoTrans[clients][products] , string nameClient[clients] , string typeProd[products] , int ClientUp)
+{
+    int cant=0 , better=0 ; 
+    for(int i =0; i<clients; i++){
+		cout<<"Nombre de Cliente: " <<nameClient[i];
+		for(int j = 0; j<products; j++ ){
+			if(pesoTrans[i][j]>13000 ){
+				cant++;
+				cout<<" Producto Transportado: "<<typeProd[j]<<endl;
+				if (cant>better){
+					better = cant;
+					ClientUp= i;
+				}
+			}
+		}
+		cant=0;
+		
+	}
 
-    //bucle para iterar sobre todos los clientes y matrices para así buscar quien ha cumplido la condición 
-    for (int i = 0; i < clients; i++)
-    {   
-        count =0; //reiniciamos el contador 
-
-        cout<<"El cliente: "<<name_clients[i]<<endl ;
-        //bucle para iterar sobre la matrices de pesos transportados y buscar quien ha transportado >1300 kg 
-        for (int j = 0; j < products; j++)
-        {   
-           //muestro los productos que cumplen el requisito
-           if(pesoTrans[i][j]>1300) 
-            {   
-                cout<<"Ha trasportado el producto: "<<type_products[j] <<" con un peso total de "<<pesoTrans[i][j]<<"kg"<<endl ; 
-                count++ ;  //contador  para saber cuantas entregas hizo el cliente mayores a 13000kg
-              
-            }
-           
-        }
-         if(count ==0 )  
-            {
-               cout<<"No ha transportado ningún producto con peso mayor a 1300.00 kg" <<endl; 
-            }   
-        if(count>mayor) //usamos esta sentencia para saber si está posición de cliente (i) es la que hizo mayores entregas  
-         {
-            mayor=count; //guardamos la variable
-            clientPosition = i; //guardamos la posición del cliente que hizo la mayor cantidad de compras para el punto 2 
-             }
-        
-    } 
-    //acá iniciamos el punto 2 donde usamos la matriz de de distanceReccory en la posición del cliente que tiene mayores entregas con 1300kg (lo sacamos del bucle de arriba) y ordenamos de forma ascendente pero creamos un array de nombres y entregas para evitar 
-    // editar el original 
-
- order_double(distanceRecorry , clientPosition , n , type_products ,  entregas ,e) ; //ejecutamos la función para ordenar las matrices y vectores de forma ascendente
-    //tenemos la posición del cliente que cumple el requisito , así que solo lo cargamos
-    cout<<"El cliente con mayor cantidad de productos que han superado los 1300kg es:  "<< name_clients[clientPosition]<<endl;
- 
- //2 bucle para mostrar las distancias recorridas  lo cúal ya están ordenadas
- for (int i = 0; i < products; i++)
- {
-  
-    cout<<" y ha transportado el producto : " << n[i]<< " una distancia total de  : "<<distanceRecorry[clientPosition][i]<<"KM"<<endl ; 
-
- }
-
-  //como la matriz está ordenada de mayor a menor  con su producto  y entregas correspondiente , entonces solo listo el último elemento del array con el producto correspondiente
-
-    cout<<"La mayor distancia recorrida fue de "<< distanceRecorry[clientPosition][products-1]<<"km"<<endl<<" siendo el producto " << n[products-1]<<endl <<"Con una cantidad total de entrega proveniente de todos los clientes : " <<e[products-1];
 }
 
+void getDistanceBetter (double distanceRecorry[clients][products] ,double distanceOrd[products] , string typeProdOrd[products] , int ClientUp ,int send)
+{
+    int better= 0 ; 
+    for(int i =0; i<products; i++){
+		cout<<"El producto : "<<typeProdOrd[i];
+		cout<<" con una distancia de : "<<distanceOrd[i]<<endl;
+		//mintras muestro, busco el mayor kilometraje
+		if( distanceRecorry[ClientUp][i]>better){
+			better=  distanceRecorry[ClientUp][i];
+			send= i;
+		}
+	}
+}
 
 int main () 
 {   //Creó las variables para trabajar 
 
-    int num_client , num_product  ;
-    string  name_clients[clients ] {"Bat" , "Bit" , "Hiru" , "Lau" , "Bost", "Sei" , "Zazpi" , "Zortzi"} , type_products [5] {"Alimenticios" , "Electrónicos" , "Metálicos" ,"Textiles" , "Químicos"} ; 
-    float peso , distance  ;
-    float pesoTrans[clients][products] {} , distanceRecorry[clients][products] {};
+    int num_client , num_product , clientUp =0 , send =0 ,cl=0 ,pr =0  ; //clientUp y send sirven para guardar al cliente q hizo más transportes de determinado kg
+    string  name_clients[clients ] {} , type_products [5] {} ,productsOrd[]{} ; 
+    string nameC ; 
+    
+    double peso , distance  ;
+    double pesoTrans[clients][products] {} , distanceRecorry[clients][products] {} , distanceRecorryOrd[products]{};
     char sep ;
     int entregas[products] {};  
    //archivo de donde vamos a empezar a sacar los datos
     ifstream archi ; 
-   
+    fstream archiNombres ; 
+    //INTRODUCIMOS LOS NOMBRES
+    archiNombres.open("Nombres.txt");
+	if(!archiNombres){
+		cout<<"No se ha encontrado el archivo";
+	}
+
+    //carga de datos de los nombres
+	while(getline(archiNombres,nameC))
+    {
+        
+        if(cl<clients){
+			
+			name_clients[cl]=nameC;
+            
+		}else{
+			type_products[pr]=nameC;
+			pr++;
+		}
+		cl++; 
+	}
+	archiNombres.close();
+
+    
 
      /*Abrimos el archivo para editarlo */
     archi.open("entrada.txt") ;
@@ -107,7 +117,7 @@ int main ()
     return 1 ; }
   
    
-    /*Ingresamos los datos al archivo*/
+    /*Ingresamos los datos al archivo Entrada*/
     cout<<"Cargando los datos . . . . . ."<<endl ; 
     /**/ while(archi>>num_client>>num_product>>peso>>distance) 
     {   
@@ -117,12 +127,15 @@ int main ()
 
     }   
     cout<<"----DATOS CARGADOS----" <<endl ; 
-    //está función resuelve el punto 2 y 3  
-    getTrans(pesoTrans, name_clients ,type_products ,distanceRecorry ,entregas);
-    
-    
     
     archi.close () ;
    
+   
+    getTransBetter(pesoTrans,name_clients,type_products,clientUp) ; 
+    order_double(distanceRecorry,distanceRecorryOrd,productsOrd , type_products,clientUp);
+    getDistanceBetter(distanceRecorry,distanceRecorryOrd,type_products,clientUp,send);
+
+    cout<<"El producto : " <<type_products[send];
+	cout<<" tiene un total de: "<<entregas[send]<<" entregas";
     return 0;
  }
